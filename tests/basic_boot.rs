f1e978faed31954ae6dde759b683fb713f11db39
harkerhand <harkerhand@outlook.com>
2025-11-12 11:58:11 +0800

    test framework

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -9,24 +9,17 @@ use op_sys::println;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     op_sys::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
